715577ad78035775b99399319283c14e9c05a89d
karankiri <karankiri.96@gmail.com>
2020-04-26 03:42:09 +0000

    Deploying to gh-pages from  @ c85dee8a792824392920b0156f6cd722aa4cf43d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53f6beed6de71d7cf35199192dbde789.js"
+  "/precache-manifest.975670d682bced2df383a7c655ce04b9.js"
 );
 
 self.addEventListener('message', (event) => {
